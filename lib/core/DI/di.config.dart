a657b0be006afb958056fd3da591f347e8f87dea
mohamed khaled <mohamedkhaledhr3050@gmail.com>
2026-05-19 21:22:24 +0300

    finished login ui and mvvm cubit with repository pattern

---
+++ lib/core/DI/di.config.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+
+import '../../data/dataSource/login_dao.dart' as _i529;
+import '../../data/dataSource_impl/login_dao_impl.dart' as _i990;
+import '../../data/repo_impl/login_repo_impl.dart' as _i886;
+import '../../repository/login_repo.dart' as _i5;
+import '../../ui/Auth/login/viewModel/login_view_model.dart' as _i610;
+
+extension GetItInjectableX on _i174.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i174.GetIt init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    gh.factory<_i529.LoginDao>(() => _i990.LoginDaoImpl());
+    gh.factory<_i5.LoginRepo>(
+          () => _i886.LoginRepoImpl(loginDao: gh<_i529.LoginDao>()),
+    );
+    gh.factory<_i610.LoginViewModel>(
+          () => _i610.LoginViewModel(gh<_i5.LoginRepo>()),
+    );
+    return this;
+  }
+}
